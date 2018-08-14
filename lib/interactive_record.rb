03cc5e526c97542f57eafabbfb6091f984de1b24
Edsel Caprice <caprice@mail.usf.edu>
2018-08-14 00:56:03 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -35,7 +35,7 @@ class InteractiveRecord
   def values_for_insert
     values = []
     self.class.col_names_for_insert.each do |column_name|
-    values << send "#'{column_name}'" if send(column_name) != nil
+    values << send "'#{send(col_name)}'" if send(column_name) != nil
   end
   values
   end
