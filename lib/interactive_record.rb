dc8c29ea98feba1405229a3e445e24e3c7a0a9e9
Edsel Caprice <caprice@mail.usf.edu>
2018-08-14 00:06:13 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -11,7 +11,12 @@ class InteractiveRecord
     DB[:conn].results_as_hash = true
     sql = "pragma table_info(?)"
     table_info = DB.[:conn].execute(sql,table_name)
-    
+    column_names = []
+    table_info.each do |row|
+      column_names << row["name"]
+    end
+    column_names.compact
+
   end
 
 
